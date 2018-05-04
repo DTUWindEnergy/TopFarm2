089b9a02a510541c5e90db7cb0ef6f86e4ccd114
Mads M. Pedersen <mmpe@dtu.dk>
2018-05-04 12:13:58 +0200

    fixed missing test files

--- tests/test_fuga/test_lib_reader.py
+++ tests/test_fuga/test_lib_reader.py
@@ -8,12 +8,13 @@ import unittest
 from topfarm.cost_models.fuga.lib_reader import read_lib
 import numpy as np
 from topfarm.cost_models.fuga import py_fuga
+from tests.test_files import testfilepath
 
 
 class Test(unittest.TestCase):
 
     def test_lib_reader(self):
-        f, A, k = read_lib(os.path.dirname(py_fuga.__file__) + "/Colonel/LUT/Farms/Horns Rev 1/hornsrev2.lib")
+        f, A, k = read_lib( testfilepath+ "wind_resources/hornsrev2.lib")
         np.testing.assert_array_almost_equal(f, [0.035972, 0.039487, 0.051674, 0.070002, 0.083645, 0.064349,
                                                  0.086432, 0.117705, 0.151576, 0.147379, 0.100121, 0.05166])
         np.testing.assert_array_almost_equal(A, [9.176929, 9.782334, 9.531809, 9.909545, 10.04269, 9.593921,
