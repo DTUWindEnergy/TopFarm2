9dbca8a67a28ad07a04d0ff9b1add405fbad545d
Mads M. Pedersen <mmpe@dtu.dk>
2018-05-24 14:30:47 +0200

    https://github.com/FUSED-Wind/FUSED-Wake/pull/13 Closes #17

--- tests/test_fusedwake_models/test_gcl.py
+++ tests/test_fusedwake_models/test_gcl.py
@@ -5,6 +5,7 @@ from topfarm.cost_models.utils.wind_resource import WindResource
 from tests.test_files import tfp
 from topfarm._topfarm import TopFarm
 import pytest
+import warnings
 
 
 @pytest.fixture()
@@ -14,24 +15,32 @@ def aep_calc():
     A = [9.176929, 9.782334, 9.531809, 9.909545]
     k = [2.392578, 2.447266, 2.412109, 2.591797]
     wr = WindResource(f, A, k, ti=np.zeros_like(f) + .1)
-    wm = FusedWakeGCLWakeModel(tfp + "wind_farms/3tb.yml")
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        wm = FusedWakeGCLWakeModel(tfp + "wind_farms/3tb.yml")
     return AEPCalculator(wr, wm)
 
 
 def test_input_shape_must_be_equal():
-    wm = FusedWakeGCLWakeModel(tfp + "wind_farms/3tb.yml")
-    with pytest.raises(AssertionError, message="Shape of no_wake_wdir, no_wake_wsp and no_wake_ti must equal"):
-        wm(wm.windFarm.pos.T, no_wake_wdir=[[270]], no_wake_wsp=[[8, 9]], no_wake_ti=0.1)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        wm = FusedWakeGCLWakeModel(tfp + "wind_farms/3tb.yml")
+        with pytest.raises(AssertionError, message="Shape of no_wake_wdir, no_wake_wsp and no_wake_ti must equal"):
+            wm(wm.windFarm.pos.T, no_wake_wdir=[[270]], no_wake_wsp=[[8, 9]], no_wake_ti=0.1)
 
 
 def test_GCL(aep_calc):
     init_pos = aep_calc.wake_model.windFarm.pos.T
-    assert aep_calc(init_pos) == 19.85973533524627  # tb aligned north-south -> wake
-    assert aep_calc(np.array([[-500, 0, 500], [0, 0, 0]]).T) == 22.31788007605505  # tb aligned West-East -> no wake
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        assert aep_calc(init_pos) == 19.85973533524627  # tb aligned north-south -> wake
+        assert aep_calc(np.array([[-500, 0, 500], [0, 0, 0]]).T) == 22.31788007605505  # tb aligned West-East -> no wake
 
 
 def test_GCL_Topfarm(aep_calc):
-    init_pos = aep_calc.wake_model.windFarm.pos.T
-    tf = TopFarm(init_pos, aep_calc.get_TopFarm_cost_component(), 160, init_pos, boundary_type='square')
-    tf.evaluate()
+    init_pos = aep_calc.wake_model.windFarm.pos
+    with warnings.catch_warnings(): # suppress "warning, make sure that this position array is oriented in ndarray([n_wt, 2]) or ndarray([n_wt, 3])"
+        warnings.simplefilter("ignore")
+        tf = TopFarm(init_pos, aep_calc.get_TopFarm_cost_component(), 160, init_pos, boundary_type='square')
+        tf.evaluate()
     assert tf.get_cost() == -19.85973533524627
