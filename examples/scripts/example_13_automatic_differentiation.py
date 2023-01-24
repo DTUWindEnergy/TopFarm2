b820ae816e561bdab40902549db09d6ed56b7b82
Mikkel Friis-Møller <mikf@dtu.dk>
2023-01-24 12:47:03 +0000

    update tilt

--- examples/scripts/example_13_automatic_differentiation.py
+++ examples/scripts/example_13_automatic_differentiation.py
@@ -8,30 +8,18 @@ from topfarm.easy_drivers import EasyScipyOptimizeDriver
 from topfarm.plotting import XYPlotComp
 
 from py_wake.deficit_models.gaussian import BastankhahGaussian
-from py_wake.turbulence_models.stf import STF2017TurbulenceModel
-from py_wake.utils import gradients
 from py_wake.utils.gradients import autograd
 from py_wake.validation.lillgrund import wt_x, wt_y, LillgrundSite, ct_curve, power_curve
-from py_wake.wind_turbines import OneTypeWindTurbines
+from py_wake.wind_turbines import WindTurbine
+from py_wake.wind_turbines.power_ct_functions import PowerCtTabular
 
 
 def main():
     if __name__ == '__main__':
-        class SWT2p3_93_65(OneTypeWindTurbines):
-            def __init__(self):
-                OneTypeWindTurbines.__init__(self, 'SWT2p3_93_65', diameter=92.6, hub_height=65,
-                                             ct_func=self._ct, power_func=self._power, power_unit='kW')
-        
-            def _ct(self, u):
-                return gradients.interp(u, ct_curve[:, 0], ct_curve[:, 1])
-        
-            def _power(self, u):
-                return gradients.interp(u, power_curve[:, 0], power_curve[:, 1])
-
+        wind_turbines = WindTurbine('SWT2p3_93_65', 92.6, 65, PowerCtTabular(ct_curve[:,0],power_curve[:,1], 'kW', ct_curve[:,1]))
         n_wt = len(wt_x)
         site = LillgrundSite()
-        wind_turbines = SWT2p3_93_65() 
-        wf_model = BastankhahGaussian(site, wind_turbines, turbulenceModel=STF2017TurbulenceModel())
+        wf_model = BastankhahGaussian(site, wind_turbines)
         constraint_comp = XYBoundaryConstraint(np.asarray([wt_x, wt_y]).T)
         cost_comp = PyWakeAEPCostModelComponent(windFarmModel=wf_model,
                                                 n_wt=n_wt,
