44c7547d0689b93f5754642c4629fc4033464c12
Ernestas Simutis <s212571@student.dtu.dk>
2025-09-26 11:41:58 +0200

    Costmodels project interface in cables notebook

--- topfarm/cost_models/economic_models/finance_wrapper.py
+++ topfarm/cost_models/economic_models/finance_wrapper.py
@@ -8,8 +8,6 @@ from costmodels.project import Project
 class OffshoreWindPlantFinanceWrapper(CostModelComponent):
     def __init__(self, windTurbines, n_wt, el_price, LIFETIME=25, **kwargs):
         cost_model = DTUOffshoreCostModel(
-            # water_depth=30.0, # DYNAMIC
-            # aep=1.0e9, # DYNAMIC
             rated_power=windTurbines.power(20) / 1e6,
             rotor_speed=10.0,
             rotor_diameter=windTurbines.diameter(),
@@ -20,9 +18,6 @@ class OffshoreWindPlantFinanceWrapper(CostModelComponent):
         )
         self.cost_model = cost_model
 
-        # out = cost_model.run(aep=373206.64521435613, water_depth=20.0)
-        # print(out)
-
         wind_technology = Technology(
             name="wind",
             lifetime=LIFETIME,
@@ -32,7 +27,7 @@ class OffshoreWindPlantFinanceWrapper(CostModelComponent):
 
         wind_farm_project = Project(
             technologies=[wind_technology],
-            product_prices={Product.SPOT_ELECTRICITY: el_price},  # np.random.uniform(0, el_price, LIFETIME)
+            product_prices={Product.SPOT_ELECTRICITY: el_price},
         )
 
         def npv_func(aep, water_depth, **kwargs):
@@ -66,16 +61,17 @@ class OffshoreWindPlantFinanceWrapper(CostModelComponent):
             water_depth_grad = grads[1][wind_technology.name]["water_depth"]
             return np.asarray(prod_grad), np.asarray(water_depth_grad)
 
-        CostModelComponent.__init__(self,
-                                    input_keys=[
-                                        ("aep", 0),
-                                        ("water_depth", 30 * np.ones(n_wt)),
-                                    ],
-                                    n_wt=n_wt,
-                                    cost_function=npv_func,
-                                    cost_gradient_function=npv_grad_func,
-                                    objective=True,
-                                    maximize=True,
-                                    output_keys=[("npv", 0)],
-                                    output_unit="EUR",
-                                    )
+        CostModelComponent.__init__(
+            self,
+            input_keys=[
+                ("aep", 0),
+                ("water_depth", 30 * np.ones(n_wt)),
+            ],
+            n_wt=n_wt,
+            cost_function=npv_func,
+            cost_gradient_function=npv_grad_func,
+            objective=True,
+            maximize=True,
+            output_keys=[("npv", 0)],
+            output_unit="EUR",
+        )
